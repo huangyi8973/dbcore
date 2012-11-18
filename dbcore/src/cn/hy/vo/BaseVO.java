bf2df1da1d388aa801b910da9494e5b74bb9de24
huangyi8973@gmail.com <huangyi8973@gmail.com@c446ca4a-05bb-bd3e-9c9a-680fefde0bab>
2012-11-18 05:22:46 +0000

    VO字段缓存

--- dbcore/src/cn/hy/vo/BaseVO.java
+++ dbcore/src/cn/hy/vo/BaseVO.java
@@ -7,14 +7,18 @@ import cn.hy.utils.StringUtils;
 
 /**
  * VO基类
+ * 
  * @version V5.0
  * @author huangy
- * @date   2012-11-15
+ * @date 2012-11-15
  */
 public abstract class BaseVO implements IBaseVO {
 
+	private String[] _fields;
+
 	/**
 	 * 获取值
+	 * 
 	 * @see cn.hy.vo.IBaseVO#getValue(java.lang.String)
 	 * @param columnName 字段名
 	 * @return 字段值
@@ -22,15 +26,15 @@ public abstract class BaseVO implements IBaseVO {
 	 * @date 2012-11-15 上午4:42:48
 	 */
 	public Object getValue(String columnName) {
-		columnName=columnName.toLowerCase();
-		Class<?> c=this.getClass();
-		Field[] fields=c.getDeclaredFields();
-		for(Field field:fields){
-			if(field.getName().equals(columnName)){
-				//有这个字段，则获取这个字段的值
-				String methodName="get"+StringUtils.firstLetterToUpper(field.getName());
+		columnName = columnName.toLowerCase();
+		Class<?> c = this.getClass();
+		Field[] fields = c.getDeclaredFields();
+		for (Field field : fields) {
+			if (field.getName().equals(columnName)) {
+				// 有这个字段，则获取这个字段的值
+				String methodName = "get" + StringUtils.firstLetterToUpper(field.getName());
 				try {
-					Method method=c.getMethod(methodName);
+					Method method = c.getMethod(methodName);
 					return method.invoke(this);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,8 +43,10 @@ public abstract class BaseVO implements IBaseVO {
 		}
 		return null;
 	}
+
 	/**
 	 * 设置值
+	 * 
 	 * @see cn.hy.vo.IBaseVO#setValue(java.lang.String, java.lang.Object)
 	 * @param columnName 字段名称
 	 * @param 字段值
@@ -48,31 +54,46 @@ public abstract class BaseVO implements IBaseVO {
 	 * @date 2012-11-15 上午4:44:03
 	 */
 	public void setValue(String columnName, Object value) {
-		columnName=columnName.toLowerCase();
-		Class<?> c=this.getClass();
-		Field[] fields=c.getDeclaredFields();//TODO 这里类的信息可以缓存起来
-		for(Field field:fields){
-			if(field.getName().equals(columnName)){
-				//有这个字段，则获取这个字段的值
-				String methodName="set"+StringUtils.firstLetterToUpper(field.getName());
+		columnName = columnName.toLowerCase();
+		Class<?> c = this.getClass();
+		Field[] fields = c.getDeclaredFields();// TODO 这里类的信息可以缓存起来
+		for (Field field : fields) {
+			if (field.getName().equals(columnName)) {
+				// 有这个字段，则获取这个字段的值
+				String methodName = "set" + StringUtils.firstLetterToUpper(field.getName());
 				try {
-					//按照函数签名的参数获取函数
-					Method method=c.getMethod(methodName,field.getType());
-					method.invoke(this,value);
+					// 按照函数签名的参数获取函数
+					Method method = c.getMethod(methodName, field.getType());
+					method.invoke(this, value);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
 	}
+
+	/**
+	 * 获得VO的字段
+	 * 
+	 * @see cn.hy.vo.IBaseVO#getFields()
+	 * @return
+	 * @author huangy
+	 * @date 2012-11-18 上午4:16:15
+	 */
 	public String[] getFields() {
-		Class<?> c=this.getClass();
-		Field[] fields=c.getDeclaredFields();
-		String[] fieldNameAry=new String[fields.length];
-		for(int i=0;i<fields.length;i++){
-			fieldNameAry[i]=fields[i].getName();
+		if (_fields == null) {
+			// 同步VO字段缓存
+			Class<?> c = this.getClass();
+			Field[] fields = c.getDeclaredFields();
+			_fields = new String[fields.length];
+			synchronized (_fields) {
+				for (int i = 0; i < fields.length; i++) {
+					_fields[i] = fields[i].getName();
+				}
+			}
+
 		}
-		return fieldNameAry;
+		return _fields;
 	}
 
 }

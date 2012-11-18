e076c6205f844321e78710a122c5c838d3d46913
huangyi8973@gmail.com <huangyi8973@gmail.com@c446ca4a-05bb-bd3e-9c9a-680fefde0bab>
2012-11-18 10:46:19 +0000

    改成静态函数

--- trunk/hybase/src/cn/hy/pub/Environment.java
+++ trunk/hybase/src/cn/hy/pub/Environment.java
@@ -10,26 +10,10 @@ import java.util.Date;
  * @date   2012-11-18
  */
 public class Environment {
-	private static Environment _instance=null;
 	private Environment(){
 		
 	}
-	/**
-	 * 获取系统环境实例
-	 * @return
-	 * @author huangy
-	 * @date 2012-11-18 上午5:59:47
-	 */
-	public static Environment getInstance(){
-		if(_instance==null){
-			synchronized (Environment.class) {
-				if(_instance==null){
-					_instance=new Environment();
-				}
-			}
-		}
-		return _instance;
-	}
+	
 	
 	/**
 	 * 获得当前时间<br/>
@@ -38,7 +22,7 @@ public class Environment {
 	 * @author huangy
 	 * @date 2012-11-18 上午6:07:51
 	 */
-	public String getCurDateTime(){
+	public static String getCurDateTime(){
 		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return df.format(new Date());
 	}
